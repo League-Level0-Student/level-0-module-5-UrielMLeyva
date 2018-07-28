4ce5c4d95d442aae4aa7518e6dff98766981e6c1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-28 10:56:44 -0700

    Circle Ring Evil Polygons Challenge

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -9,19 +9,45 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+		Robot r2d2 = new Robot();
+			
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		r2d2.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "What color would you like?", "Colored Polygons", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"RED","BLUE", "BLACK","GREEN","CYAN"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
+		if(colorChoice == 0) {
+			r2d2.setPenColor(Color.RED);
+		}
+		if(colorChoice == 1) {
+			r2d2.setPenColor(Color.BLUE);
+		}
+		if(colorChoice == 2) {
+			r2d2.setPenColor(Color.BLACK);
+		}
+		if(colorChoice == 3) {
+			r2d2.setPenColor(Color.GREEN);
+		}
+		if(colorChoice == 4) {
+			r2d2.setPenColor(Color.CYAN);
+		}
+		//4. Ask the user how many polygons they want to be drawn.
+		String p = JOptionPane.showInputDialog("How many polygons would you like to draw?");
 		
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		int polygon = Integer.parseInt(p);
+		r2d2.penDown();
+			r2d2.setX(150);
+			r2d2.setY(300);
+		for (int i = 0; i < polygon; i++) {
+			for (int j = 0; j < 8; j++) {
+				r2d2.move(50);
+				r2d2.turn(360/15);
+			}
+			r2d2.turn(60);
+		}
 		//6. Make it so your shapes do not overlap
-
+		
 		//7. Challenge: add more colors to the Option Dialog.
 	}
 }
