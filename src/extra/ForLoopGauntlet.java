70ed1faa2213e34006fd53ef0c64853e8a7b27b1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-15 10:53:50 -0700

    For Loop Gauntlet

--- src/extra/ForLoopGauntlet.java
+++ src/extra/ForLoopGauntlet.java
@@ -3,15 +3,52 @@ package extra;
 public class ForLoopGauntlet {
 public static void main(String[] args) {
 	
-for(int i = 0; i < 100; i++);
+	//for(int i = 0; i < 101; i++) {
+		//system.out.println(i);
+	///}
+	
+	
+	//for (int i = 100; i > 0; i--) {
+		//System.out.println(i);
+	//}
+	
+	
+	//for (int i = 2; i < 102; i+=2) {
+		//System.out.println(i);
+	//}
+	
+	
+	//for (int i = 1; i < 101; i+=2) {
+		//System.out.println(i);
+	//}
 
 
+	//for (int i = 1; i < 501; i++) {
+		
+		//if(i%2==0) {
+			//System.out.println(i+" is even");
+		//}
+		//else {
+			//System.out.println(i+" is odd");
+	//}
+	//}
 
+	//for (int i = 0; i < 778; i+=7) {
+		//System.out.println(i);
+	//}
 
-
-
-
-
+	//for (int i = 2003; i < 2019; i++) {
+		//int a = i-2003;
+		//System.out.println("In "+i+" I was "+a+" years old");
+	//}
+	System.out.println();
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			
+		System.out.println(i +" "+ j);	
+		}
+		
+	}
 
 
 
