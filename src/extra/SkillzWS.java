e5e985c74989b0dcd0b2488fa8aa2176e87679b9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-17 10:52:55 -0800

    Skillz WS

---
+++ src/extra/SkillzWS.java
@@ -0,0 +1,45 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class SkillzWS {
+public static void main(String[] args) {
+	
+String s = JOptionPane.showInputDialog("What school do you go to?");
+	
+	JOptionPane.showMessageDialog(null, ""+ s +" is a fantastic school.");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}}
